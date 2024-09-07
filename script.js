a70edd76761fae7ac07e38a2e93b72aa8fb9f9f9
Rohit Kumar <130774573+rohitkumar-dev@users.noreply.github.com>
2024-09-07 18:11:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ console.log(countryList);
 for(let select of dropdowns){
     for(currCode in countryList){
         let newOption = document.createElement("option")
-        newOption.innerText = currCode
-        newOption.value = currCode
+        newOption.innerText = `${currCode}`
+        newOption.value = `${currCode}`
 
         if(select.name==="from" && currCode==="INR"){
             newOption.selected="selected"
@@ -69,5 +69,42 @@ const updateRate = async () => {
     let final_amt = (amt_val * rate).toFixed(2)
     msg.innerText = `${amt_val} ${fromCurr.value} = ${final_amt} ${toCurr.value}`
 }
+
+
+function swap(){
+  
+  const from_select = document.querySelector(".from-box select")
+  const from_option = from_select.options[from_select.selectedIndex]
+  
+  const from_curr = from_option.innerText
+  const from_curr_val = from_option.value
+  const from_img = document.querySelector(".from-box img").src
+
+  
+  const to_select = document.querySelector(".to-box select")
+  const to_option = to_select.options[to_select.selectedIndex]
+  
+  const to_curr = to_option.innerText
+  const to_curr_val = to_option.value
+  const to_img = document.querySelector(".to-box img").src
+  
+  //swaping
+  from_option.innerText = to_curr;
+  from_option.value = to_curr_val;
+  document.querySelector(".from-box img").src = to_img;
+  
+  to_option.innerText = from_curr;
+  to_option.value = from_curr_val;
+  document.querySelector(".to-box img").src = from_img;
+
+  console.log(from_img, from_curr, from_curr_val, to_img, to_curr, to_curr_val)
+}
+
+const swaper = document.querySelector("#swaper")
+swaper.addEventListener("click", (evt) => {
+  evt.preventDefault()
+  swap()
+  updateRate()
+})
 
 
